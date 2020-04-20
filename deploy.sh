bc74ee0f96ad8f7d7bdf137e577595e9d7897b76
hamxayaxin <hamxayaxin@gmail.com>
2020-04-20 16:58:14 +0500

    multn services

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-echo "This will stop and remove ERC20 container, then remove ERC20 image and create and run new image"
+echo "This will stop and remove erc container, then remove erc image and create and run new image"
 
-echo "Stoping docker for ERC20...."
-docker stop  ERC20-service
+echo "Stoping docker for erc...."
+docker stop  erc-service
 
-echo "Removing docker for ERC20...."
-docker rm  ERC20-service
+echo "Removing docker for erc...."
+docker rm  erc-service
 
-echo "Removing docker image for ERC20...."
-docker image rm  ERC20-services-image:latest
-
-
-echo "Building docker image for ERC20...."
-docker build -t ERC20-services-image:latest .
+echo "Removing docker image for erc...."
+docker image rm  erc-services-image:latest
 
 
-echo "Running docker image for ERC20...."
+echo "Building docker image for erc...."
+docker build -t erc-services-image:latest .
+
+
+echo "Running docker image for erc...."
 docker run -d --name erc-service --restart=always -p 5000:5000 -e NODE_ENV=production erc-services-image:latest
